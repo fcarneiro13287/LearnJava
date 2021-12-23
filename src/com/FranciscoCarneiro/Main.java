9b707e10b1486e1793fd437fbd21c50c436e33fc
fcarneiro13287 <francisco.jose.carneiro@gmail.com>
2021-12-23 18:06:57 +0000

    versao 2

--- src/com/FranciscoCarneiro/Main.java
+++ src/com/FranciscoCarneiro/Main.java
@@ -3,9 +3,10 @@ package com.FranciscoCarneiro;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
         System.out.println("=======================");
         System.out.println("hello world");
+        System.out.println("O dia está de chuva");
         System.out.println("=======================");
     }
 }
